3d1e7ea97669abf4aa9d3a5aaac43727b48face7
breinersn <bsuarezn1@unicartagena.edu.co>
2020-07-07 03:57:08 -0500

    service-worker, pwa, firebase, bootstrap, fontAwe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import {  AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+
+  firebaseConfig : {
+    apiKey: 'AIzaSyBqFZ0dpcrod18pktZPeaW5udbkUxFeHcE',
+    authDomain: 'angularpwa-2563c.firebaseapp.com',
+    databaseURL: 'https://angularpwa-2563c.firebaseio.com',
+    projectId: 'angularpwa-2563c',
+    storageBucket: 'angularpwa-2563c.appspot.com',
+    messagingSenderId: '626720849526',
+    appId: '1:626720849526:web:4304d4e17b370cba93e957'
+  },
   production: true
 };
