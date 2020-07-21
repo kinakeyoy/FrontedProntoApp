55bca03abd0530a5eb6fb83bc171eaf64372a5c3
breinersn <bsuarezn1@unicartagena.edu.co>
2020-07-21 18:37:41 -0500

    validacion form registro

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { IUser } from '../interfaces/usuario';
 
 @Component({
   selector: 'app-registro',
@@ -9,9 +11,38 @@ export class RegistroComponent implements OnInit {
 
   hide = true;
 
-  constructor() { }
+  addressForm = this.fb.group({
+    email: [null, Validators.compose([
+      Validators.required,
+      Validators.maxLength(100),
+      Validators.pattern('[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}')
+    ])
+    ],
+    password: [null, Validators.compose([
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(100)
+    ])
+    ],
+    displayName: [null, Validators.compose([
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ])
+    ]
+  });
+
+  user: IUser = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  registro() {
+    console.log(this.user);
+  }
 }
